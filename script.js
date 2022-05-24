c8d235a84701a826e93758a9a3f1a82c941a0c27
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-05-24 16:38:06 +0200

    bonus immagine

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const mess = new Vue ({
     el: "#container",
     data: {
         messaggio: "hello word with vue js",
-        Image: "img/omer-haktan-bulut--_LKIdqHo3w-unsplash.jpg"
+        Image: "img/omer-haktan-bulut--_LKIdqHo3w-unsplash.jpg",
     },
+    methods: {
+        change: function () {
+            let counter = 0;
+            if (counter == 0) {
+                this.Image = "img/witchkingblogs-xZDk453dHbg-unsplash.jpg"
+                counter++;
+                console.log(counter);
+            } else {
+                this.Image = "img/omer-haktan-bulut--_LKIdqHo3w-unsplash.jpg"
+            }
+         }
+    }
 });

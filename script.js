996059edaf3ae205085d0cc9e6409b98f8fd98fa
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-05-24 16:13:06 +0200

    inserimento immagine

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const mess = new Vue ({
     el: "#container",
     data: {
         messaggio: "hello word with vue js",
-        
+        Image: "https://unsplash.com/photos/-_LKIdqHo3w"
     },
 });

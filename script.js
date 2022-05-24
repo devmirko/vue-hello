78b6ee121363219809bb0c7ed6ba741764bbe876
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-05-24 17:02:56 +0200

    inserimento carousel

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
+
 const mess = new Vue ({
     el: "#container",
     data: {
         messaggio: "hello word with vue js",
         Image: "img/omer-haktan-bulut--_LKIdqHo3w-unsplash.jpg",
+        counter: 0,
     },
     methods: {
         change: function () {
-            let counter = 0;
-            if (counter == 0) {
-                this.Image = "img/witchkingblogs-xZDk453dHbg-unsplash.jpg"
-                counter++;
-                console.log(counter);
-            } else {
-                this.Image = "img/omer-haktan-bulut--_LKIdqHo3w-unsplash.jpg"
-            }
-         }
+           
+      switch (this.counter) {
+          case 0:
+              this.Image ="img/witchkingblogs-xZDk453dHbg-unsplash.jpg";
+              this.counter++;
+              break;
+           case 1:
+            this.Image ="img/tezos-5NX5x2-O7KU-unsplash.jpg";
+            this.counter++;
+             break;
+             case 2:
+            this.Image ="img/mingrui-he-SOxmqV_a3PU-unsplash.jpg";
+            this.counter++;
+             break;
+         
+      }
     }
+  }
 });

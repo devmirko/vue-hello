58d62845730ff3b3c80eec9f852d0718ad88c041
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-05-24 17:18:20 +0200

    corousel finito da stilizzare

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ const mess = new Vue ({
         messaggio: "hello word with vue js",
         Image: "img/omer-haktan-bulut--_LKIdqHo3w-unsplash.jpg",
         counter: 0,
+        classimage: "image"
     },
     methods: {
-        change: function () {
+        change_right: function () {
            
       switch (this.counter) {
           case 0:
@@ -22,8 +23,29 @@ const mess = new Vue ({
             this.Image ="img/mingrui-he-SOxmqV_a3PU-unsplash.jpg";
             this.counter++;
              break;
+            
          
       }
-    }
+    },
+
+    change_left: function () {
+           
+        switch (this.counter) {
+            case 3:
+                this.Image ="img/tezos-5NX5x2-O7KU-unsplash.jpg";
+                this.counter--;
+                break;
+             case 2:
+              this.Image ="img/witchkingblogs-xZDk453dHbg-unsplash.jpg";
+              this.counter--;
+               break;
+               case 1:
+              this.Image ="img/omer-haktan-bulut--_LKIdqHo3w-unsplash.jpg";
+              this.counter--;
+               break;
+              
+           
+        }
+      }
   }
 });
